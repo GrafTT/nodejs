8188e944833ed4f5949a195ec25a1d4ffb1067d9
Viacheslav Lamash <slava91billion@gmail.com>
2018-12-02 22:00:10 +0400

    Homework-1

---
+++ app.js
@@ -0,0 +1,9 @@
+import config from './config/config'
+
+import {User, Product} from './models'
+
+console.log(config.name)
+
+new User()
+new Product()
+
---
+++ config/config.js
@@ -0,0 +1,2 @@
+export default {"name": "Node.js Homework Application"}
+
