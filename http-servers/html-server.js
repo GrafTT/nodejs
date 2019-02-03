fb919bbe19834427d656aea7d361522e1a8c6f6d
unknown <slava91billion@gmail.com>
2019-02-03 15:51:44 +0400

    Servers

---
+++ http-servers/html-server.js
@@ -0,0 +1,10 @@
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'plain/text'
+    })
+    res.end('Hello World')
+})
+
+server.listen(8080)
